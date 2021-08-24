5514d25e4adafa9e24949c9aa1c71e14225362fe
Dom McCabe <dommct@yahoo.co.uk>
2021-08-24 21:41:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+let url = "https://run.mocky.io/v3/dfe80d90-c9d6-4add-bd64-a1fbaa1b5f73";
+
+let newReq = new Request(url);
+
+fetch(newReq)
+  .then((response) => response.json())
+  .then((data) => {
+   console.log(data);
+  });
+
+  /*
+   var campaign1 = document.getElementById("campaigns");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.innerHTML =
+        "Name: " +
+        data[i].name +
+        " " +
+        "Offer begins:" +
+        data[i].starts_at +
+        " " +
+        "Offer ends:" +
+        data[i].ends_at;
+
+      mainContainer.appendChild(div);
+      console.log("test");
+    }
+    */
